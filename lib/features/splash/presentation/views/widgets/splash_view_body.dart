21a3f2a42b548b80fbc9e10335543def0e054567
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-11-26 04:40:32 +0200

    Use go router to navigate to home screen

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,9 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:bookly/features/splash/presentation/views/widgets/sliding_image.dart';
 import 'package:bookly/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -67,10 +68,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
 
   void navigateToHome() {
     Future.delayed(const Duration(seconds: 3), () {
-      Get.to(
-        () => const HomeView(),
-        transition: Transition.circularReveal,
-      );
+      GoRouter.of(context).push(AppRouter.kHomeView);
     });
   }
 }
