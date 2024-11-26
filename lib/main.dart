6f2cad934c09db734ebb33a4a34382ceeab64b2b
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-11-26 04:40:11 +0200

    Use router instead of get material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:bookly/constants.dart';
-import 'package:bookly/features/splash/presentation/views/splash_view.dart';
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -18,19 +17,23 @@ class Bookly extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: const Size(360, 690),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        builder: (context, child) {
-          return GetMaterialApp(
-            theme: ThemeData.dark()
-                .copyWith(scaffoldBackgroundColor: kPrimaryColor ,
-                textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
-            debugShowCheckedModeBanner: false,
-            useInheritedMediaQuery: true,
-            home: child,
-          );
-        },
-        child: const SplashView());
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp.router(
+          routerConfig: AppRouter.router,
+          theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: kPrimaryColor,
+            textTheme: GoogleFonts.montserratTextTheme(
+              ThemeData.dark().textTheme,
+            ),
+          ),
+          debugShowCheckedModeBanner: false,
+        );
+      },
+    );
   }
 }
+
+
