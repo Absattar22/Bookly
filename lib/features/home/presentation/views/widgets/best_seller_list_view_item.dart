f6852e7c030144ee19b1fe3da140d7e220cea338
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-11-29 20:30:27 +0200

    Make the list clickable

--- lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,81 +1,91 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125.h,
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 12.w,
-          vertical: 12.h,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            // Book Image
-            Container(
-              width: 95.w,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8.r),
-                color: Colors.red,
-                image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(AssestData.testImage),
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(
+          AppRouter.kBookDetails,
+        );
+      },
+      child: SizedBox(
+        height: 125.h,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 12.w,
+            vertical: 12.h,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              // Book Image
+              Container(
+                width: 95.w,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.r),
+                  color: Colors.red,
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(AssestData.testImage),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(width: 30.w),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Harry Potter and the Goblet of Fire',
-                    style: Styles.textStyle20.copyWith(
-                      fontFamily: 'Sectra',
-                    ),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  SizedBox(
-                    height: 3.h,
-                  ),
-
-                  // Author Name
-                  Text(
-                    'J.K. Rowling',
-                    style: Styles.textStyle14,
-                  ),
-                  SizedBox(
-                    height: 3.h,
-                  ),
-                  Row(
-                    children: [
-                      // Price
-                      Text(
-                        '19.99 €',
-                        style: Styles.textStyle20.copyWith(
-                          fontWeight: FontWeight.bold,
-                        ),
+              SizedBox(width: 30.w),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Harry Potter and the Goblet of Fire',
+                      style: Styles.textStyle20.copyWith(
+                        fontFamily: 'Sectra',
                       ),
-                      const Spacer(),
-                      const BookRating(),
-                    ],
-                  ),
-                ],
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(
+                      height: 3.h,
+                    ),
+
+                    // Author Name
+                    Text(
+                      'J.K. Rowling',
+                      style: Styles.textStyle14,
+                    ),
+                    SizedBox(
+                      height: 3.h,
+                    ),
+                    Row(
+                      children: [
+                        // Price
+                        Text(
+                          '19.99 €',
+                          style: Styles.textStyle20.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const Spacer(),
+                        const BookRating(),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
+      
     );
   }
 }
