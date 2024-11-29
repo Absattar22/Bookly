074bae540659770d121973459deb70653b424b9f
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-11-29 19:12:03 +0200

    Add Book details view route

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
@@ -5,6 +6,7 @@ import 'package:go_router/go_router.dart';
 abstract class AppRouter {
   static const kHomeView = '/homeView';
   static const kSplashView = '/';
+  static const kBookDetails = '/bookDetailsView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -14,7 +16,11 @@ abstract class AppRouter {
       GoRoute(
         path: kHomeView,
         builder: (context, state) => const HomeView(),
-      )
+      ),
+      GoRoute(
+        path: kBookDetails,
+        builder: (context, state) => const BookDetailsView(),
+      ),
     ],
   );
 }
