5cab72d9e3045fb997cb42a34c317e3589adbb01
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-12-01 00:16:46 +0200

    Finish the book details view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,15 +1,33 @@
+import 'package:bookly/features/home/presentation/views/widgets/book_details_item.dart';
+import 'package:bookly/features/home/presentation/views/widgets/books_actions.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
+import 'package:bookly/features/home/presentation/views/widgets/related_books.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CustomBookDetailsAppBar(),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Scaffold(
+            body: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.h),
+              child: const Column(
+                children: [
+                  CustomBookDetailsAppBar(),
+                  BookDetailsItem(),
+                  BooksActions(),
+                  RelatedBooks()
+                ],
+              ),
+            ),
+          ),
+        )
       ],
     );
   }
