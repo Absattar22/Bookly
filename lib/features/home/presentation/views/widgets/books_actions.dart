6201aa0593bb3556a11373ac654ce04e98d35dd2
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-12-01 00:17:09 +0200

    Add Padding to the row

--- lib/features/home/presentation/views/widgets/books_actions.dart
+++ lib/features/home/presentation/views/widgets/books_actions.dart
@@ -1,37 +1,41 @@
 import 'package:bookly/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class BooksActions extends StatelessWidget {
   const BooksActions({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      children: [
-        Expanded(
-          child: CustomButton(
-            text: '19.19 €',
-            backGroundColor: Color.fromARGB(255, 255, 255, 255),
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(16),
-              bottomLeft: Radius.circular(16),
+    return Padding(
+      padding: EdgeInsets.only(bottom: 30.h),
+      child: const Row(
+        children: [
+          Expanded(
+            child: CustomButton(
+              text: '19.19 €',
+              backGroundColor: Color.fromARGB(255, 255, 255, 255),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16),
+                bottomLeft: Radius.circular(16),
+              ),
+              textColor: Color.fromARGB(255, 0, 0, 0),
             ),
-            textColor: Color.fromARGB(255, 0, 0, 0),
           ),
-        ),
-        Expanded(
-          child: CustomButton(
-            fontSize: 16,
-            text: 'Free Preview',
-            backGroundColor: Color(0xffEF8262),
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(16),
-              bottomRight: Radius.circular(16),
+          Expanded(
+            child: CustomButton(
+              fontSize: 16,
+              text: 'Free Preview',
+              backGroundColor: Color(0xffEF8262),
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(16),
+                bottomRight: Radius.circular(16),
+              ),
+              textColor: Color.fromARGB(255, 255, 255, 255),
             ),
-            textColor: Color.fromARGB(255, 255, 255, 255),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
