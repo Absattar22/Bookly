ee829127315876e611ca61f248f6022aeecb9091
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-12-01 01:30:02 +0200

    Remove the padding from the text field

--- lib/features/search/presentation/views/widgets/custom_text_field.dart
+++ lib/features/search/presentation/views/widgets/custom_text_field.dart
@@ -19,62 +19,56 @@ class _CustomTextFieldState extends State<CustomTextField> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(
-        vertical: 30.h,
-        horizontal: 10.w,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(
-              left: 8.w,
-              right: 8.w,
-              bottom: 8.h,
-            ),
-            child: TextField(
-              style: const TextStyle(
-                color: Colors.white,
-              ),
-              controller: controller,
-              onChanged: widget.onChanged,
-              cursorColor: Colors.white,
-              autocorrect: true,
-              decoration: InputDecoration(
-                  filled: true,
-                  fillColor: const Color.fromARGB(255, 30, 30, 30),
-                  contentPadding: EdgeInsets.symmetric(
-                    vertical: 15.h,
-                    horizontal: 10.w,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: const BorderSide(
-                      color: Color.fromARGB(255, 21, 123, 40),
-                    ),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.r),
-                    borderSide: const BorderSide(
-                      color: Color.fromARGB(255, 43, 41, 56),
-                    ),
-                  ),
-                  hintText: 'Enter book title',
-                  hintStyle: const TextStyle(
-                    color: Color.fromARGB(255, 126, 126, 126),
-                  ),
-                  labelStyle: const TextStyle(
-                    color: Colors.white,
-                  ),
-                  prefixIcon: Icon(
-                    FontAwesomeIcons.magnifyingGlass,
-                    size: 15.w,
-                  )),
-            ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(
+            left: 8.w,
+            right: 8.w,
+            bottom: 8.h,
           ),
-        ],
-      ),
+          child: TextField(
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+            controller: controller,
+            onChanged: widget.onChanged,
+            cursorColor: Colors.white,
+            autocorrect: true,
+            decoration: InputDecoration(
+                filled: true,
+                fillColor: const Color.fromARGB(255, 30, 30, 30),
+                contentPadding: EdgeInsets.symmetric(
+                  vertical: 15.h,
+                  horizontal: 10.w,
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: const BorderSide(
+                    color: Color.fromARGB(255, 21, 123, 40),
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.r),
+                  borderSide: const BorderSide(
+                    color: Color.fromARGB(255, 43, 41, 56),
+                  ),
+                ),
+                hintText: 'Enter book title',
+                hintStyle: const TextStyle(
+                  color: Color.fromARGB(255, 126, 126, 126),
+                ),
+                labelStyle: const TextStyle(
+                  color: Colors.white,
+                ),
+                prefixIcon: Icon(
+                  FontAwesomeIcons.magnifyingGlass,
+                  size: 15.w,
+                )),
+          ),
+        ),
+      ],
     );
   }
 }
