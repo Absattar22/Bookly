eb18e55ee73ecc7b6bd671b844626ea9f06e5226
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-12-01 01:31:06 +0200

    Finish The Search View and the ui is done

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,10 +1,52 @@
+import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/search/presentation/views/widgets/custom_text_field.dart';
+import 'package:bookly/features/search/presentation/views/widgets/search_reslut_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.only(
+              top: 20.h,
+              right: 10.w,
+              left: 10.w,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomTextField(),
+                SizedBox(
+                  height: 10.h,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 8.w),
+                  child: Text(
+                    'Search Results',
+                    style: Styles.textStyle18.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return const SearchReslutListView();
+            },
+            childCount: 1,
+          ),
+        ),
+      ],
+    );
   }
 }
