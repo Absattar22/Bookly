3695881aa4c68dc7a0247f224007f2411d1e7168
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-12-01 02:40:00 +0200

    Fetch newest books

---
+++ lib/features/home/data/repos/home_repo_implementaion.dart
@@ -0,0 +1,34 @@
+import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/core/utils/api_service.dart';
+import 'package:bookly/features/home/data/model/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImplementaion implements HomeRepo {
+  final ApiService apiService;
+  HomeRepoImplementaion(this.apiService);
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+        endPoint:
+            'volumes?Filtering=free-ebooks&q=subject:programming&Sorting=newest',
+      );
+      List<BookModel> books = [];
+
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return Right(books);
+    } catch (e) {
+      return Left(ServerFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeauteredBooks() {
+    // TODO: implement fetchFeauteredBooks
+    throw UnimplementedError();
+  }
+}
