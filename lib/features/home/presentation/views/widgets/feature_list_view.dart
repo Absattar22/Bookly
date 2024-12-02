8bd68db374775bb5fcfc249dac69b7b693127261
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-12-02 02:51:00 +0200

    Add the bloc to the featured list view and handles the states

--- lib/features/home/presentation/views/widgets/feature_list_view.dart
+++ lib/features/home/presentation/views/widgets/feature_list_view.dart
@@ -1,24 +1,57 @@
+import 'package:bookly/features/home/presentation/view%20model/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class FeatureListView extends StatelessWidget {
   const FeatureListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 180.h,
-      child: ListView.builder(
-          itemCount: 20,
-          shrinkWrap: true,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(8).w,
-              child: const CustomBookItem(),
-            );
-          }),
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksFailure) {
+          return Center(
+            child: Text(
+              state.errMessage,
+              style: TextStyle(color: Colors.red, fontSize: 16.sp),
+            ),
+          );
+        } else if (state is FeaturedBooksSuccess) {
+          return SizedBox(
+            height: 180.h,
+            child: ListView.builder(
+              itemCount: state.books.length,
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.all(8.w),
+                  child: const CustomBookItem(),
+                );
+              },
+            ),
+          );
+        }
+        return SizedBox(
+          height: 180.h,
+          child: ListView.builder(
+            itemCount: 20,
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: EdgeInsets.all(8.w),
+                child: const Skeletonizer(
+                  child: CustomBookItem(),
+                ),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
