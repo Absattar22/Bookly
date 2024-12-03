36306c4edee3891bb60fae3de933c85c34fb2167
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-12-04 01:15:15 +0200

    Fetch the data from api ad display it

--- lib/features/home/presentation/views/widgets/books_actions.dart
+++ lib/features/home/presentation/views/widgets/books_actions.dart
@@ -1,19 +1,23 @@
+import 'package:bookly/core/functions/launch_url.dart';
 import 'package:bookly/core/widgets/custom_button.dart';
+import 'package:bookly/features/home/data/model/book_model/book_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class BooksActions extends StatelessWidget {
-  const BooksActions({super.key});
+  const BooksActions({super.key, required this.bookModel});
+
+  final BookModel bookModel;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(bottom: 30.h),
-      child: const Row(
+      child: Row(
         children: [
-          Expanded(
+          const Expanded(
             child: CustomButton(
-              text: '19.19 €',
+              text: 'Free',
               backGroundColor: Color.fromARGB(255, 255, 255, 255),
               borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(16),
@@ -24,18 +28,30 @@ class BooksActions extends StatelessWidget {
           ),
           Expanded(
             child: CustomButton(
+              onPressed: () async {
+                await launchCustomUrl(
+                    context, bookModel.volumeInfo.previewLink);
+              },
               fontSize: 16,
-              text: 'Free Preview',
-              backGroundColor: Color(0xffEF8262),
-              borderRadius: BorderRadius.only(
+              text: getText(bookModel),
+              backGroundColor: const Color(0xffEF8262),
+              borderRadius: const BorderRadius.only(
                 topRight: Radius.circular(16),
                 bottomRight: Radius.circular(16),
               ),
-              textColor: Color.fromARGB(255, 255, 255, 255),
+              textColor: const Color.fromARGB(255, 255, 255, 255),
             ),
           ),
         ],
       ),
     );
+  }
+
+  String getText(BookModel bookModel) {
+    if (bookModel.volumeInfo.previewLink == null) {
+      return 'Not available';
+    } else {
+      return 'Preview';
+    }
   }
 }
