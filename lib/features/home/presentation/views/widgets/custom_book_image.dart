ac39f0c96a2a6925e6524599870aa33a86973b6f
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-12-04 01:07:54 +0200

    Make the height and the width dynamic

--- lib/features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/features/home/presentation/views/widgets/custom_book_image.dart
@@ -3,22 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomBookImage extends StatelessWidget {
-  const CustomBookImage({super.key, required this.imgUrl});
+  const CustomBookImage(
+      {super.key,
+      required this.imgUrl,
+      required this.width,
+      required this.height});
 
   final String imgUrl;
+  final int width, height;
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(16.r),
       child: CachedNetworkImage(
-        height: 140.h,
-        width: 140.w,
+        height: height.h,
+        width: width.h,
         imageUrl: imgUrl,
         errorWidget: (context, url, error) => Container(
           color: Colors.grey.shade300,
-          height: 180.h,
-          width: 140.w,
+          height: 170.h,
+          width: 110.w,
           child: const Icon(Icons.error, color: Colors.red),
         ), // Error widget for failed image loading
         fit: BoxFit.fill,
