27085795d314b67923042adaa98eccdc17e658e4
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-12-03 02:42:34 +0200

    Provide Related books cubit the view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,7 +1,12 @@
+import 'package:bookly/core/utils/service_locator.dart';
+import 'package:bookly/features/home/data/model/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo_implementaion.dart';
+import 'package:bookly/features/home/presentation/view%20model/related_books_cubit/related_books_cubit.dart';
 import 'package:bookly/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:bookly/features/search/presentation/views/search_view.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
@@ -21,7 +26,14 @@ abstract class AppRouter {
       ),
       GoRoute(
         path: kBookDetails,
-        builder: (context, state) => const BookDetailsView(),
+        builder: (context, state) => BlocProvider(
+          create: (context) => RelatedBooksCubit(
+            getIt.get<HomeRepoImplementaion>(),
+          ),
+          child:  BookDetailsView(
+            bookModel: state.extra as BookModel,
+          ),
+        ),
       ),
       GoRoute(
         path: kBookSearch,
