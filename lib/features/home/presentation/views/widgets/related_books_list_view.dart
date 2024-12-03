8c0fc91fc1776cf48e021991103135230445a4a4
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-12-03 02:45:27 +0200

    Provide states and the return widgets

--- lib/features/home/presentation/views/widgets/related_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/related_books_list_view.dart
@@ -1,5 +1,8 @@
+import 'package:bookly/core/skeletonizers/related_books_skeletonizer.dart';
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/features/home/presentation/view%20model/related_books_cubit/related_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class RelatedBooksListView extends StatelessWidget {
@@ -7,32 +10,40 @@ class RelatedBooksListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 120.h,
-      child: ListView.builder(
-        itemCount: 20,
-        shrinkWrap: true,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsets.only(right: 12.h),
-            child: Container(
-              height: 150.h,
-              width: 90.w,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16).w,
-                color: const Color.fromARGB(255, 255, 255, 255),
-                image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(
-                    AssestData.testImage,
+    return BlocBuilder<RelatedBooksCubit, RelatedBooksState>(
+      builder: (context, state) {
+        if (state is RelatedBooksSuccess) {
+          return SizedBox(
+            height: 120.h,
+            child: ListView.builder(
+              itemCount: state.books.length,
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.only(right: 12.h),
+                  child: Container(
+                    height: 150.h,
+                    width: 90.w,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16).w,
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      image: const DecorationImage(
+                          fit: BoxFit.fill, image: NetworkImage('imgUrl')),
+                    ),
                   ),
-                ),
-              ),
+                );
+              },
             ),
           );
-        },
-      ),
+        } else if (state is RelatedBooksFailure) {
+          return Center(
+            child: Text(state.errMessage),
+          );
+        } else {
+          return const RelatedBooksSkeletonizer();
+        }
+      },
     );
   }
 }
