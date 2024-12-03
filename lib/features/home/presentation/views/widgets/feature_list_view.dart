cc06c0021fb55b67ba8b5cfd5f1057a525049751
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-12-04 01:04:51 +0200

    Manage feature list view clicks

--- lib/features/home/presentation/views/widgets/feature_list_view.dart
+++ lib/features/home/presentation/views/widgets/feature_list_view.dart
@@ -1,9 +1,11 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/features/home/presentation/view%20model/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:bookly/core/skeletonizers/skeletonizer_book_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
 class FeatureListView extends StatelessWidget {
   const FeatureListView({super.key});
@@ -21,7 +23,7 @@ class FeatureListView extends StatelessWidget {
           );
         } else if (state is FeaturedBooksSuccess) {
           return SizedBox(
-            height: 180.h,
+            height: 170.h,
             child: ListView.builder(
               physics: const BouncingScrollPhysics(),
               itemCount: state.books.length,
@@ -32,7 +34,17 @@ class FeatureListView extends StatelessWidget {
                     state.books[index].volumeInfo.imageLinks?.thumbnail ?? '';
                 return Padding(
                   padding: EdgeInsets.all(8.w),
-                  child: CustomBookImage(imgUrl: thumbnail),
+                  child: GestureDetector(
+                    onTap: () {
+                      GoRouter.of(context).push(AppRouter.kBookDetails,
+                          extra: state.books[index]);
+                    },
+                    child: CustomBookImage(
+                      height: 170,
+                      width: 110,
+                      imgUrl: thumbnail,
+                    ),
+                  ),
                 );
               },
             ),
