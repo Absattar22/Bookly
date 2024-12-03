e590909f20e5d24a79c4971ab31ab46ba6128228
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-12-04 00:59:31 +0200

    Create a search cubit

---
+++ lib/features/search/view model/search_books_cubit/search_books_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/features/home/data/model/book_model/book_model.dart';
+import 'package:bookly/features/search/presentation/views/widgets/data/repos/search_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'search_books_state.dart';
+
+class SearchBooksCubit extends Cubit<SearchBooksState> {
+  SearchBooksCubit(this.searchRepo) : super(SearchBooksInitial());
+  final SearchRepo searchRepo;
+
+  Future<void> searchBooks({required String title}) async {
+    emit(SearchBooksLoading());
+    var result = await searchRepo.searchBooks(title: title);
+
+    result.fold(
+      (failure) {
+        emit(SearchBooksFailure(errMessage:  failure.errMessage));
+      },
+      (books) {
+        emit(SearchBooksSuccess(books: books));
+      },
+    );
+  }
+}
