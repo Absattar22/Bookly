0c3ba1fef56fcb4c2bdf5e6e3704029d0e07041c
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-12-04 01:16:49 +0200

    Change text style from 30 to 24

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -9,19 +9,18 @@ abstract class Styles {
   static TextStyle textStyle20 = TextStyle(
     fontSize: 20.sp,
     fontWeight: FontWeight.normal,
-
   );
   static TextStyle textStyle14 = TextStyle(
     fontSize: 14.sp,
     fontWeight: FontWeight.normal,
   );
-   static TextStyle textStyle16 = TextStyle(
+  static TextStyle textStyle16 = TextStyle(
     fontSize: 16.sp,
     fontWeight: FontWeight.w500,
   );
-  static TextStyle textStyle30 = TextStyle(
-    fontSize: 30.sp,
+  static TextStyle textStyle24 = TextStyle(
+    fontSize: 24.sp,
     fontWeight: FontWeight.normal,
-     fontFamily: 'Sectra'
+    fontFamily: 'Sectra',
   );
 }
