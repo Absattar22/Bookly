998b8a4cfd865f0c8ce84bd10a68d0989d3e178b
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-12-04 01:14:02 +0200

    Fetch the data from the api

--- lib/features/home/presentation/views/widgets/book_details_item.dart
+++ lib/features/home/presentation/views/widgets/book_details_item.dart
@@ -1,39 +1,63 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/home/data/model/book_model/book_model.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
-import 'package:bookly/features/home/presentation/views/widgets/custom_book_item.dart';
+import 'package:bookly/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
 class BookDetailsItem extends StatelessWidget {
-  const BookDetailsItem({super.key});
+  const BookDetailsItem({super.key, required this.book});
+
+  final BookModel book;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 30.h),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const CustomBookItem(),
-          SizedBox(height: 35.h),
-          Text(
-            'The Jungle Book',
-            style: Styles.textStyle30.copyWith(color: Colors.white),
-          ),
-          SizedBox(height: 2.h),
-          Text(
-            ' J.K. Rowling',
-            style: Styles.textStyle18.copyWith(
-                color: const Color.fromARGB(255, 189, 188, 188),
-                fontWeight: FontWeight.w500,
-                fontStyle: FontStyle.italic),
-          ),
-          SizedBox(
-            height: 6.h,
-          ),
-          const BookRating()
-        ],
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(
+          AppRouter.kBookDetails,
+          extra: book,
+        );
+      },
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 30.h),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomBookImage(
+              height: 190,
+              width: 150,
+              imgUrl: book.volumeInfo.imageLinks?.thumbnail ?? '',
+            ),
+            SizedBox(height: 25.h),
+            Center(
+              child: Text(
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                book.volumeInfo.title ?? '',
+                style: Styles.textStyle24.copyWith(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(height: 2.h),
+            Center(
+              child: Text(
+                textAlign: TextAlign.center,
+                book.volumeInfo.authors?[0] ?? '',
+                style: Styles.textStyle18.copyWith(
+                    color: const Color.fromARGB(255, 189, 188, 188),
+                    fontWeight: FontWeight.w500,
+                    fontStyle: FontStyle.italic),
+              ),
+            ),
+            BookRating(count: book.volumeInfo.pageCount)
+          ],
+        ),
       ),
     );
   }
