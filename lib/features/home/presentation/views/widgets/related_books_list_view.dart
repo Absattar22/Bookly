bc03085e9e3589d70f5dd556bc795b0571d4946e
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-12-04 01:03:47 +0200

    Mange related books to open the details for each one of them

--- lib/features/home/presentation/views/widgets/related_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/related_books_list_view.dart
@@ -1,9 +1,11 @@
 import 'package:bookly/core/skeletonizers/related_books_skeletonizer.dart';
-import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/features/home/presentation/view%20model/related_books_cubit/related_books_cubit.dart';
+import 'package:bookly/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
 class RelatedBooksListView extends StatelessWidget {
   const RelatedBooksListView({super.key});
@@ -21,18 +23,19 @@ class RelatedBooksListView extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               itemBuilder: (context, index) {
                 return Padding(
-                  padding: EdgeInsets.only(right: 12.h),
-                  child: Container(
-                    height: 150.h,
-                    width: 90.w,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16).w,
-                      color: const Color.fromARGB(255, 255, 255, 255),
-                      image: const DecorationImage(
-                          fit: BoxFit.fill, image: NetworkImage('imgUrl')),
-                    ),
-                  ),
-                );
+                    padding: EdgeInsets.only(right: 12.h),
+                    child: GestureDetector(
+                      onTap: () {
+                        GoRouter.of(context).push(AppRouter.kBookDetails,
+                            extra: state.books[index]);
+                      },
+                      child: CustomBookImage(
+                          height: 150,
+                          width: 90,
+                          imgUrl: state.books[index].volumeInfo.imageLinks
+                                  ?.thumbnail ??
+                              ''),
+                    ));
               },
             ),
           );
