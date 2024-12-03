71ff2a8abb7cc30c18c8861720fc254044c38de5
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-12-03 02:19:44 +0200

    Implement fetch related books

--- lib/features/home/data/repos/home_repo_implementaion.dart
+++ lib/features/home/data/repos/home_repo_implementaion.dart
@@ -62,4 +62,31 @@ class HomeRepoImplementaion implements HomeRepo {
       );
     }
   }
+  
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchRelatedBooks({required String category}) async {
+    try {
+      var data = await apiService.get(
+        endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming&Sorting=relevance',
+      );
+      List<BookModel> books = [];
+
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return Right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return Left(
+          ServerFailure.fromDioException(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
+  }
 }
