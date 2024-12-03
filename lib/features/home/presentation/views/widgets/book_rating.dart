4859ab6d160a1d172b5ea2198ec7e23ef033649d
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-12-03 02:13:40 +0200

    Get the page count from api

--- lib/features/home/presentation/views/widgets/book_rating.dart
+++ lib/features/home/presentation/views/widgets/book_rating.dart
@@ -1,12 +1,15 @@
+import 'package:bookly/core/utils/random_number.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
+  const BookRating({super.key, required this.count});
+  final int count;
 
   @override
   Widget build(BuildContext context) {
+    double rating = getRandomDouble(3, 5);
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -16,13 +19,13 @@ class BookRating extends StatelessWidget {
         ),
         SizedBox(width: 4.w),
         Text(
-          '4.8',
+          rating.toStringAsFixed(1),
           style: Styles.textStyle16
               .copyWith(color: Colors.white, fontWeight: FontWeight.w600),
         ),
         SizedBox(width: 4.w),
         Text(
-          '(2300)',
+          '($count)',
           style: Styles.textStyle14
               .copyWith(color: const Color.fromARGB(255, 172, 171, 171)),
         ),
