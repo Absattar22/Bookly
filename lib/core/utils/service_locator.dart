20abdf4feb239aa4d6c768773bf83cd17750cbcc
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-12-04 01:12:28 +0200

    Add service locator for search repo implementation

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/features/home/data/repos/home_repo_implementaion.dart';
+import 'package:bookly/features/search/presentation/views/widgets/data/repos/search_repo_implementation.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
@@ -8,8 +9,12 @@ final getIt = GetIt.instance;
 void setupServiceLocator() {
   getIt.registerSingleton<ApiService>(ApiService(Dio()));
   getIt.registerSingleton<HomeRepoImplementaion>(
-    HomeRepoImplementaion(
-      getIt.get<ApiService>()
+    HomeRepoImplementaion(getIt.get<ApiService>(),),
+  );
+
+  getIt.registerSingleton<SearchRepoImplementation>(
+    SearchRepoImplementation(
+      apiService: getIt.get<ApiService>(),
     ),
   );
 }
